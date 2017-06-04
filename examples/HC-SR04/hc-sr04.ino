144001241d97150ebad47939d8456ac70c2290b7
nouc <nouc23@gmail.com>
2017-06-04 16:01:26 +0200

    add sample

---
+++ examples/HC-SR04/hc-sr04.ino
@@ -0,0 +1,51 @@
+#include <Sampler.h>
+
+#define trigPin 5
+#define echoPin 4
+
+Sampler sampler(40, 4, 200);
+
+void setup() {
+  Serial.begin(9600);
+}
+
+void loop() {
+  float measure = singleMeasure();
+  
+  sampler.addSample(measure);
+
+  // print raw measure
+  Serial.print(measure);
+  Serial.print(" ");
+
+  // print invesed value for graph
+  Serial.print(
+    sampler.getMeasurement() * -1
+  );
+  
+  Serial.println("");
+  
+  delay(50);
+
+}
+
+float singleMeasure() {
+
+  long time;
+  float distance;
+
+  // generate triger
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(2);
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(1);
+  digitalWrite(trigPin, LOW);
+
+  // measure time
+  time = pulseIn(echoPin, HIGH, 30000);
+
+  // calculate distance
+  distance = time / 58.0;
+
+  return distance;
+}
